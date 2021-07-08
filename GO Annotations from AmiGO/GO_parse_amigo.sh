fc50b416a29592a1ac4cb3315ff6295bc2198a43
meadm <44445181+meadm@users.noreply.github.com>
2021-07-08 15:53:58 -0500

    Create GO_parse_amigo.sh

---
+++ GO Annotations from AmiGO/GO_parse_amigo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#start the while loop, read in each line of the domain id file, and define a variable "IDS" that is each line of the domain id file
+while read GOID; do
+#paste command converts the column of text to a row of text that is separated by a tab - make sure the spaces between the quotes are still a tab and not simple spaces
+#sed command adds the GO id to the front of every line - make sure the spaces after the variable are a tab and not regular spaces
+	grep "$GOID" Aflav_Amigo_GO3.txt | cut -f1,1 | sort -u | paste -sd"	" - | sed "s/^/$GOID	/" > $GOID.ids2
+done < GO_Amigo_ids.txt
+cat *ids2 > Aflav_Amigo_GO_parsed.txt
